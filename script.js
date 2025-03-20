488d393dbe983765b91aac03f6108a6a5d833912
Siddharth Singh <193688659+sid207794@users.noreply.github.com>
2025-03-20 21:26:10 +0000

    Create constants 'triquetra', 'logo', 'logoText' and two event listeners with properties within for 'triquetra'

--- script.js
+++ script.js
@@ -15,3 +15,19 @@ theme.addEventListener("click", () => {
     const newTheme = root.className === "dark" ? "light" : "dark";
     root.className = newTheme;
 });
+
+const triquetra = document.querySelector("#triquetra");
+const logo = document.querySelector(".logo");
+const logoText = document.querySelector(".logoText");
+
+triquetra.addEventListener("mouseover", () => {
+    logo.style.paddingLeft = "25px";
+    logo.style.transition = "translateX(-25px)";
+    logo.style.clipPath = "inset(0 443px 0 0)";
+});
+
+triquetra.addEventListener("mouseout", () => {
+    logo.style.paddingLeft = "55px";
+    logo.style.transition = "translateX(0px)";
+    logo.style.clipPath = "inset(0 0 0 0)";
+});
