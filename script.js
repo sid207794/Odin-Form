f4072a7c246921f6c4af3f7360505ec52631e1ce
Siddharth Singh <193688659+sid207794@users.noreply.github.com>
2025-03-24 11:23:09 +0000

    Replace the unit 'px' and their associated numeric value of all properties into their corresponding values with units in 'rem', 'vh', or 'vw' within both the 'triquetra' event handlers

--- script.js
+++ script.js
@@ -21,13 +21,13 @@ const logo = document.querySelector(".logo");
 const logoText = document.querySelector(".logoText");
 
 triquetra.addEventListener("mouseover", () => {
-    logo.style.paddingLeft = "25px";
-    logo.style.clipPath = "inset(0 443px 0 0)";
+    logo.style.paddingLeft = "1.627vw"; /*25px*/
+    logo.style.clipPath = "inset(0 28.84vw 0 0)"; /*443px*/
     logo.style.transition = "padding-left 0.6s ease-in-out, clip-path 0.6s ease-in-out";
 });
 
 triquetra.addEventListener("mouseout", () => {
-    logo.style.paddingLeft = "55px";
+    logo.style.paddingLeft = "3.2vw"; /*55px*/
     logo.style.clipPath = "inset(0 0 0 0)";
     logo.style.transition = "padding-left 0.6s ease-in-out, clip-path 0.6s ease-in-out";
 });
